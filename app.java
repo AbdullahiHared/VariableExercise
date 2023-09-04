0b08115d882baf3f43c2daf441bc81b8829a5567
Abdullahi <muxsinxareed6@gmail.com>
2023-09-04 14:22:03 +0200

    Empty commit

--- app.java
+++ app.java
@@ -13,22 +13,27 @@ public class app {
         int subtract = num1 - num2;
 
         System.out.println("The sum of num2 och num2 is: " + sum);
+        System.out.println();
 
         System.out.println("The subtraction of num1 and num2: " + subtract);
+        System.out.println();
 
         int product = num1 * num2;
         int quota = num1 / num2;
 
         System.out.println("The product of num1 and num2 is: " + product);
+        System.out.println();
+
         System.out.println("The quota of num1/num2 is: " + quota);
+        System.out.println();
 
         int remainder1 = 10%3;
         int remainder2 = 173%2;
 
         System.out.println("The remainder of 10 % 3 is = " + remainder1);
+        System.out.println();
         System.out.println("The remainder of 173 % 2 is = " + remainder2);
-
-
+        System.out.println();
 
         double decimalNumOne = 5.3;
         double decimalNumTwo = 44.4;
@@ -37,12 +42,17 @@ public class app {
         double subtractDecimal = decimalNumOne  - decimalNumTwo;
 
         System.out.println("The sum of decimalOne and decimalTwo is: " + addDecimal);
+
+        System.out.println();
         System.out.println("The subtraction of decimalOne and decimalTwo is: " + subtractDecimal);
+        System.out.println();
 
         double multiplyDecimal = decimalNumOne  + decimalNumTwo;
         double divideDecimal = decimalNumOne  + decimalNumTwo;
+    
 
         System.out.println("The division of decimalNumOne and decimalNumTwo is: " + divideDecimal);
+        System.out.println();
         System.out.println("The multiplication of decimalNumOne and decimalNumTwo is:" + multiplyDecimal);
 
 
