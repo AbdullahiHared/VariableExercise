6dde2bb60d4811c3e5e206ead77c1583595f5224
Abdullahi <muxsinxareed6@gmail.com>
2023-09-04 12:02:31 +0200

    "add and subtract"

---
+++ app.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class app {
+
+    public static void main (String[] args) {
+        Scanner newScanner = new Scanner(System.in);
+
+        int num1 = 10;
+        int num2 = 3;
+
+        int sum = num1 + num2;
+
+        int subtract = num1 - num2;
+
+        System.out.println("The sum of num2 och num2 is: " + sum);
+
+        System.out.println("The subtraction of num1 and num2: " + subtract);
+
+        
+
+
+
+
+    }
+}
